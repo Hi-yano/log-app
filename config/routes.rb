6b12cf723a043cd6254453bab6651f2057b2cca8
Hi-yano <73208511+Hi-yano@users.noreply.github.com>
2020-11-25 16:28:41 +0900

    ユーザー管理機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'logs/index'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'logs#index'
+  resources :users, only: [:edit, :update]
 end
